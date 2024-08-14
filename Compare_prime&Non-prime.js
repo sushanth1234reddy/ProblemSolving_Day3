6b403a812f56d693154fd35cacaabac2ec0adbe9
Singadwar Sushanth Reddy <76097402+sushanth1234reddy@users.noreply.github.com>
2024-08-14 11:27:46 +0530

    Add files via upload

---
+++ Compare_prime&Non-prime.js
@@ -0,0 +1,32 @@
+let number=String(7498926889);
+primesum=0;
+nonprimesum=0;
+for(num of number){
+    if(num>1){
+        count=0;
+        for(i=2;i<num;i++){
+            if(num%i==0){
+                nonprimesum=nonprimesum+Number(num);
+                count++;
+                break;
+            }
+        }
+        if(count==0){
+            primesum=primesum+Number(num);
+        }
+    }
+}
+console.log("Sum of all prime numbers is:",primesum);
+console.log("Sum of all Non-Primes is:",nonprimesum);
+if(primesum>nonprimesum){
+    console.log("primesum is bigger than Nonprimesum");
+}
+else if(primesum==nonprimesum){
+    console.log("primesum is equal to Nonprimesum");
+}
+else{
+    console.log("Nonprimesum is bigger than primesum");
+}
+
+
+// summ(number);
